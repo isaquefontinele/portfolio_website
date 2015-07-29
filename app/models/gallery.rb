6eb1e471a09da8c08bb592083291a558a45e3e9f
Isaque Fontinele <isaquefontinele@gmail.com>
2015-07-29 13:09:29 -0700

    Added gallery, album and photo. Routes updated.

---
+++ app/models/gallery.rb
@@ -0,0 +1,5 @@
+class Gallery < ActiveRecord::Base
+
+  has_many :albums
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
-  #get 'welcome' => 'welcome#index'
+
+  root 'welcome#index'
 
   get 'about' => 'welcome#about'
-
   get 'contact' => 'welcome#contact'
+
+  resources :galleries
+  resources :albums
+  resources :photos
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'welcome#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
